61685c41500c27e08ff2ebf8f2dd114ed0768d1a
Claire DeBoer <claire.deboer@gmail.com>
2020-10-27 22:31:13 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,11 +2,9 @@ def find_max_value(array)
   count = 0
   max_value = -1
   while count < array.length do
-    
+    if (array[count] > max_value)
+      max_value = array[count]
     count += 1
   end
-end
-
-def find_max_value(array)
-  array.max
+  max_value
 end
